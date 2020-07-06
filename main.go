6bfca49b91d6fcf9053cd733985659a59dc763ca
Gautier Mathon <contact@gautier.ovh>
2020-07-06 21:51:26 +0200

    Improve logging and handle HTTP GET timeout

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"git.gautier.ovh/hosts2dnsmasq/dnsmasq"
 	"git.gautier.ovh/hosts2dnsmasq/hosts"
@@ -14,10 +15,11 @@ import (
 const (
 	version               = "v1.0.0-rc.1"
 	dnsmasqConfigFileMode = 0600
+	hostsGetTimeout       = 30 * time.Second
 )
 
 func main() {
-	hostFileURL := flag.String("host-url", hosts.DefaultURL, "Host file URL")
+	hostsFileURL := flag.String("host-url", hosts.DefaultURL, "Host file URL")
 	confFilePath := flag.String("conf-path", "conf", "Config file path")
 	confFileFormat := flag.String("conf-format", dnsmasq.DefaultConfigFormat, "Which format to use while writing the configuration")
 	showVersion := flag.Bool("version", false, "Print binary version")
@@ -29,16 +31,21 @@ func main() {
 		return
 	}
 
-	r, err := hosts.Download(&http.Client{}, *hostFileURL)
+	log.Printf("Downloading hosts file %s", *hostsFileURL)
+	client := &http.Client{Timeout: hostsGetTimeout}
+	r, err := hosts.Download(client, *hostsFileURL)
 	if err != nil {
-		log.Printf("Failed to download hosts file from '%s': %s", *hostFileURL, err)
+		log.Printf("Failed to download hosts file from '%s': %s", *hostsFileURL, err)
 		os.Exit(1)
 	}
+
+	log.Print("Parsing hosts file")
 	hosts, err := hosts.Parse(r)
 	if err != nil {
 		log.Printf("Failed to parse hosts file: %s", err)
 		os.Exit(1)
 	}
+	log.Printf("Parsed %d hosts", len(hosts))
 
 	configFile, err := os.OpenFile(*confFilePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, dnsmasqConfigFileMode)
 	if err != nil {
@@ -46,6 +53,7 @@ func main() {
 	}
 	defer configFile.Close()
 
+	log.Printf("Writing dnsmasq config to '%s'", *confFilePath)
 	err = dnsmasq.WriteConfig(configFile, *confFileFormat, hosts)
 	if err != nil {
 		log.Printf("Failed to write Dnsmasq configuration: %s", err)
