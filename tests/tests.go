9095b20fed3b46af84ab2d5c9d9be62545cdebd6
Gautier Mathon <contact@gautier.ovh>
2020-07-06 21:38:28 +0200

    linting

--- tests/tests.go
+++ tests/tests.go
@@ -1,4 +1,8 @@
 package tests
 
-var Hosts = []string{"testD0main.com", "an0thertestdomain.com"}
-var Config = "address=/testD0main.com/0.0.0.0\naddress=/an0thertestdomain.com/0.0.0.0\n"
+var (
+	// Hosts is a hosts list example
+	Hosts = []string{"testD0main.com", "an0thertestdomain.com"}
+	// Config is the dnsmasq config file that should be generated with Hosts
+	Config = "address=/testD0main.com/0.0.0.0\naddress=/an0thertestdomain.com/0.0.0.0\n"
+)
